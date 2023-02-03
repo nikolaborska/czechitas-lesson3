912eafb1338bdad9c3830f73d148cc1ae9fff273
Nikola Borska <m-nikol.svedova@email.cz>
2023-02-03 13:55:54 +0100

    změna

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 // tady je místo pro náš program
+
+// zjištění jména a jeho výpis do HTML
+
+let jmeno = document.querySelector(".jmeno");
+let jmenoUzivatele = prompt("Zadej své celé jméno: ");
+
+jmeno.innerHTML = jmenoUzivatele;
+
+// zjištění věku a jeho výpis do HTML
+let vek = document.querySelector(".vek");
+let vekUzivatele = Number(prompt("Zadej svůj věk: "));
+
+vek.innerHTML = vekUzivatele;
+
+//výpis věty do HTML 
+document.body.innerHTML += "<p>" + "Jméno a příjmení: " + ", " + jmenoUzivatele + " " + "Věk: " + vekUzivatele + "</p>";
